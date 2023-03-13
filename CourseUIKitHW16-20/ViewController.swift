74539091c49452d7d9ba1bf43ad774bc922ae561
DenisKurlykov <dkurlykov@gmail.com>
2023-03-13 18:06:42 +0400

    Initial Commit

---
+++ CourseUIKitHW16-20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CourseUIKitHW16-20
+//
+//  Created by Денис Курлыков on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

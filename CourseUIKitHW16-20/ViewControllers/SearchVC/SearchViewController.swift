dec27777df759d0e307c80b2eb8f61182e84d9bb
DenisKurlykov <dkurlykov@gmail.com>
2023-03-16 21:43:17 +0400

    ДЗ 16 выполнено

--- CourseUIKitHW16-20/ViewControllers/SearchVC/SearchViewController.swift
+++ CourseUIKitHW16-20/ViewControllers/SearchVC/SearchViewController.swift
@@ -7,8 +7,28 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-   
+final class SearchViewController: UIViewController {
+    
+    private let clearButton = UIButton(type: .system)
+    
+    private lazy var recentlyViewedLabels = label(text: "Недавно просмотренные", fontSize: 22)
+    private lazy var requestOptionsLabel = label(text: "Варианты запросов", fontSize: 22)
+    
+    private let productCardOneView = ProductCard(
+        textLabel: """
+Беспроводные наушники
+Apple AirPods Max,
+серебристый
+""",
+        textSize: 15,
+        imageName: "Airpods Max_1"
+    )
+    
+    private lazy var firstLineSearchTextField = textField(text: "AirPods", textSize: 22)
+    private lazy var secondLineSearchTextField = textField(text: "AppleCare", textSize: 22)
+    private lazy var thirdLineSearchTextField = textField(text: "Beats", textSize: 22)
+    private lazy var fourthLineSearchTextField = textField(text: "Beats", textSize: 22)
+    
     // MARK: - Override Methods
     
     override func viewDidLoad() {
@@ -16,6 +36,8 @@ class SearchViewController: UIViewController {
         overrideUserInterfaceStyle = .dark
         
         setupNavigationBar()
+        setupClearButton()
+        setupUI()
     }
     
     // MARK: - Private Methods
@@ -27,5 +49,71 @@ class SearchViewController: UIViewController {
         let searchController = UISearchController()
         navigationItem.searchController = searchController
         searchController.searchBar.placeholder = "Поиск по продуктам и магазинам"
+    }
+    
+    private func setupClearButton() {
+        clearButton.setTitle("Очистить", for: .normal)
+        clearButton.titleLabel?.font = .systemFont(ofSize: 20)
+    }
+    
+}
+
+// MARK: - SetupUI
+
+private extension SearchViewController {
+    func setupUI() {
+        addSubviews(
+            recentlyViewedLabels,
+            clearButton,
+            productCardOneView,
+            requestOptionsLabel,
+            firstLineSearchTextField,
+            secondLineSearchTextField,
+            thirdLineSearchTextField,
+            fourthLineSearchTextField
+        )
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        [
+            recentlyViewedLabels,
+            clearButton,
+            productCardOneView,
+            requestOptionsLabel,
+            firstLineSearchTextField,
+            secondLineSearchTextField,
+            thirdLineSearchTextField,
+            fourthLineSearchTextField
+        ].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        
+        NSLayoutConstraint.activate([
+            recentlyViewedLabels.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            recentlyViewedLabels.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            
+            clearButton.centerYAnchor.constraint(equalTo: recentlyViewedLabels.centerYAnchor),
+            clearButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            
+            productCardOneView.heightAnchor.constraint(equalToConstant: 200),
+            productCardOneView.widthAnchor.constraint(equalToConstant: 150),
+            productCardOneView.topAnchor.constraint(equalTo: recentlyViewedLabels.bottomAnchor, constant: 16),
+            productCardOneView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            
+            requestOptionsLabel.topAnchor.constraint(equalTo: productCardOneView.bottomAnchor, constant: 32),
+            requestOptionsLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            
+            firstLineSearchTextField.topAnchor.constraint(equalTo: requestOptionsLabel.bottomAnchor, constant: 8),
+            firstLineSearchTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            
+            secondLineSearchTextField.topAnchor.constraint(equalTo: firstLineSearchTextField.bottomAnchor, constant: 8),
+            secondLineSearchTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            
+            thirdLineSearchTextField.topAnchor.constraint(equalTo: secondLineSearchTextField.bottomAnchor, constant: 8),
+            thirdLineSearchTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            
+            fourthLineSearchTextField.topAnchor.constraint(equalTo: thirdLineSearchTextField.bottomAnchor, constant: 8),
+            fourthLineSearchTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8)
+        ])
     }
 }

7c212beba2a1e18123da54d9aff818ab8107c06c
DenisKurlykov <dkurlykov@gmail.com>
2023-04-09 13:05:55 +0400

    Создана тень для карточки текущего продукта к доставке

--- CourseUIKitHW16-20/ViewControllers/ForYouVC/ForYouViewController.swift
+++ CourseUIKitHW16-20/ViewControllers/ForYouVC/ForYouViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 final class ForYouViewController: UIViewController {
 
+    // MARK: - IBOutlet
+    
+    @IBOutlet var currentDeliveryProductView: UIView!
+    
+    // MARK: - Override Method
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -17,9 +23,22 @@ final class ForYouViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         overrideUserInterfaceStyle = .light
         setupTabBar()
+        setupShadow()
     }
+    
+    // MARK: - Private Method
     
     private func setupTabBar() {
         tabBarController?.tabBar.backgroundColor = .white
+    }
+    
+    private func setupShadow() {
+        currentDeliveryProductView.layer.cornerRadius = 15
+        
+        currentDeliveryProductView.layer.shadowColor = UIColor.gray.cgColor
+        currentDeliveryProductView.layer.shadowOpacity = 0.5
+        currentDeliveryProductView.layer.shadowRadius = 10
+        currentDeliveryProductView.layer.shadowOffset = CGSize(width: 0, height: 10)
+    
     }
 }
